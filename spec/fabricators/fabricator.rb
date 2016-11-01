f6392843af6870f1f1ba637fd38e197fb288ec08
Lee Saville <l.w.saville@gmail.com>
2016-11-01 08:34:50 -0600

    Add fabricator for category

--- spec/fabricators/fabricator.rb
+++ spec/fabricators/fabricator.rb
@@ -3,4 +3,9 @@ Fabricator(:item) do
   description { Faker::Company.bs }
   price { Faker::Commerce.price }
   image { Faker::Avatar.image}
+  category
+end
+
+Fabricator(:category) do
+  title { Faker::Team.name }
 end
