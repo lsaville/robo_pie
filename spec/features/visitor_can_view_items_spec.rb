fb6dd5f5a492eac9987c6d9f03b210716b8d9839
Jon Kimble <jonathanbkimble@gmail.com>
2016-10-31 19:36:24 -0600

    finish items index

--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe "visitor views item index" do
   scenario "visitor can see all items" do
+    Item.create(title: "Pizza 1", description: "so so good", price: 15, image: "www.image.com")
+    Item.create(title: "Pizza 2", description: "so so good", price: 15, image: "www.image.com")
+    Item.create(title: "Pizza 3", description: "so so good", price: 15, image: "www.image.com")
+
     visit items_path
 
     expect(page).to have_content("Pizza 1")
