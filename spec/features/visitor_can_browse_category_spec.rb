51bb9644426c429255a1a32b7839069de6b01cbc
Lee Saville <l.w.saville@gmail.com>
2016-11-01 08:43:30 -0600

    Create feature test for visitor browses category:

---
+++ spec/features/visitor_can_browse_category_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Visitor can browse by category" do
+  context "Categories exist" do
+    describe "they visit /:category_name" do
+      category1 = Category.create(title: "gasket lovers")
+      category2 = Category.create(title: "plastitarian")
+
+      item1 = Item.create(title: "gaskets", description: "lots of gaskets", price: 15, image: "some image", category: category1)
+      item2 = Item.create(title: "flummox", description: "great stuff", price: 15, image: "image", category: category1)
+
+      item3 = Item.create(title: "rabbits", description: "oil soaked", price: 15, image: "sea of oil", category: category2)
+      item4 = Item.create(title: "lego castle", description: "legos", price: 15, image: "junk yard", category: category2)
+
+      visit "/gasket_lovers"
+
+      expect(page).to have_content item1.title
+      expect(page).to have_content item2.title
+      expect(page).to_not have_content item3.title
+      expect(page).to_not have_content item4.title
+    end
+  end
+end
