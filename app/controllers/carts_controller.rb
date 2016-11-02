74f7407991789d6a52c8f0478d0d389bfd709fe8
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-01 18:32:54 -0600

    Start poro implementation

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,10 +2,9 @@ class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
   def create
     item = Item.find(params[:item_id])
-    cart = session[:cart]
+    @cart.add_item(item.id)
+    session[:cart] = @cart.contents
     flash[:notice] = "You now have #{pluralize(1, item.title)}"
-
     redirect_to items_path
-
   end
 end
