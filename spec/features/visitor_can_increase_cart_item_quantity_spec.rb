ebe53293f10d83043358dcf06ea41a386b38b4f0
Lee Saville <l.w.saville@gmail.com>
2016-11-02 17:04:11 -0600

    Create test for story 6

---
+++ spec/features/visitor_can_increase_cart_item_quantity_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'User increases item quantity in cart' do
+  scenario 'they increase the item quantity for an item in cart' do
+    category = Fabricate(:category)
+    @item = Fabricate(:item, category: category)
+    visit items_path
+    click_button 'Add To Cart'
+
+    visit cart_path
+
+    within('.quantity') do
+      expect(page).to have_content("1")
+    end
+
+    select '4', :from => 'quantity_select'
+
+    expect(current_path).to eq(cart_path)
+    within('.quantity') do
+      expect(page).to have_content("4")
+    end
+    expect(page).to have_content("#{item.price * 4}")
+  end
+end
