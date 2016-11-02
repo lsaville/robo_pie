8177a4258e0be7c0569c6d721793f5158c592369
Lee Saville <l.w.saville@gmail.com>
2016-11-02 14:55:26 -0600

    Add custom delete path for /cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index]
 
   get "/cart", to: 'carts#show'
+  delete "/cart", to: 'carts#destroy'
   get ":category_title", to: 'categories#show'
 
 end
