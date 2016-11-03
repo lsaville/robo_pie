757db184747769a5947e485c50408c70cbe9c5ee
Lee Saville <l.w.saville@gmail.com>
2016-11-03 15:16:54 -0600

    Add fabricator for user

--- spec/fabricators/fabricator.rb
+++ spec/fabricators/fabricator.rb
@@ -9,3 +9,9 @@ end
 Fabricator(:category) do
   title { Faker::Team.name }
 end
+
+Fabricator(:user) do
+  name { Faker::Name.name }
+  email { Faker::Internet.email }
+  password { Faker::Internet.password }
+end
