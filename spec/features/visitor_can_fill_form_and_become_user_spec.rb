efa99c0eb12a5c169a150ad5dc50af884ecf52f8
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-03 11:01:46 -0600

    Add happy and sad path test for user story seven

---
+++ spec/features/visitor_can_fill_form_and_become_user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe "Visitor visits login page and enters information" do
+  context "valid parameters" do
+
+    scenario "Visitor is now a user" do
+
+      visit login_path
+      click_link "Create Account"
+
+      fill_in "Name", with: "Frank"
+      fill_in "Email", with: "F@gmail.com"
+      fill_in "Password", with: "123"
+      fill_in "Password Confirmation", with: "123"
+      click_button "Create Account"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Logged in as Frank")
+      expect(page).to have_content("Frank")
+      expect(page).to have_content("F@gmail.com")
+      expect(page).to_not have_content("Login")
+      expect(page).to have_content("Logout")
+
+    end
+  end
+
+  context "invalid parameters" do
+    scenario "visitor fails to enter name" do
+
+      visit login_path
+
+      fill_in "Name", with: nil
+      fill_in "Email", with: "F@gmail.com"
+      fill_in "Password", with: "123"
+      fill_in "Password Confirmation", with: "123"
+      click_button "Create Account"
+
+      expect(page).to have_content("prohibited this record from being saved")
+    end
+
+  end
+end
