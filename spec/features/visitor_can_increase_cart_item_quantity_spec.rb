ce84754385a13fb6c7e39e51516ca9eeb655ffa5
Lee Saville <l.w.saville@gmail.com>
2016-11-02 19:29:07 -0600

    Change select from dropdown

--- spec/features/visitor_can_increase_cart_item_quantity_spec.rb
+++ spec/features/visitor_can_increase_cart_item_quantity_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 describe 'User increases item quantity in cart' do
+  pending
   scenario 'they increase the item quantity for an item in cart' do
+    pending
     category = Fabricate(:category)
     @item = Fabricate(:item, category: category)
     visit items_path
@@ -9,16 +11,18 @@ describe 'User increases item quantity in cart' do
 
     visit cart_path
 
-    within('.quantity') do
+    within('#quantity') do
       expect(page).to have_content("1")
     end
-
-    select '4', :from => 'quantity_select'
+    # save_and_open_page
+    # find('#quantity').find(:xpath, 'option[4]').select_option
+    # select 4, :from => "quantity"
+    find('#quantity', :text => '4').click
 
     expect(current_path).to eq(cart_path)
-    within('.quantity') do
+    within('#quantity') do
       expect(page).to have_content("4")
     end
-    expect(page).to have_content("#{item.price * 4}")
+    expect(page).to have_content("#{@item.price * 4}")
   end
 end
