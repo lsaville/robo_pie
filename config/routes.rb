8fd658dc6be35b6f4d57dad00fcf2321b677394f
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-02 20:27:36 -0600

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  root to: "welcome#show"
+  get "/login", to: 'users#login'
 
   resources :carts, only: [:create]
-
-  resources :items, only: [:index, :show]
-
   get "/cart", to: 'carts#show'
   delete "/cart", to: 'carts#destroy'
   put "/cart", to: 'carts#update'
+
+  resources :items, only: [:index, :show]
+
+
   get ":category_title", to: 'categories#show'
+
 
 end
