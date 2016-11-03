5868d59e099901d9c35abe60254448cf970efce4
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-03 11:07:09 -0600

    Add private params to create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
 
-  def login
+  def dashboard
+
+  end
+
+  def new
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to dashboard_path
+    else
+      render :login
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
