ce211c7790a936d45128b64f5270af26ccce7310
Lee Saville <l.w.saville@gmail.com>
2016-11-04 11:51:49 -0600

    Add route for carts to index

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :new]
 
+  resources :orders, only: [:index]
+
   resources :carts, only: [:create]
   get '/cart', to: 'carts#show'
   delete '/cart', to: 'carts#destroy'
@@ -16,8 +18,5 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index, :show]
 
-
   get ':category_title', to: 'categories#show'
-
-
 end
