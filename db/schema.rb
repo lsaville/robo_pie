f45c8bd561e832da419b5c28b9812485e92e9ec2
Lee Saville <l.w.saville@gmail.com>
2016-11-03 19:13:54 -0600

    Add orders table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103141208) do
+ActiveRecord::Schema.define(version: 20161104011232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20161103141208) do
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -41,4 +48,5 @@ ActiveRecord::Schema.define(version: 20161103141208) do
   end
 
   add_foreign_key "items", "categories"
+  add_foreign_key "orders", "users"
 end
