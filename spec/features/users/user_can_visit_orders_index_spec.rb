ce988d6018fe3c0c1de73ac96067065bb0750d61
Lee Saville <l.w.saville@gmail.com>
2016-11-03 19:17:06 -0600

    Start order index test

---
+++ spec/features/users/user_can_visit_orders_index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'User can see past orders' do
+  scenario 'they visit /orders and can see just their past orders' do
+    category = Fabricate(:category)
+    items = Fabricate.times(5, :item, category: category)
+    user = User.create(name: 'r', email: 'r@r.com', password: '123')
+    order1 = Order.create(user: user)
+    order1.items.new(items[0])
+    byebug
+
+    visit login_path
+    fill_in 'Email', with: 'r@r.com'
+    fill_in 'Password', with: '123'
+
+
+  end
+end
