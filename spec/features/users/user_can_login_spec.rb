91db5c76b0026228d010bdc70163ddf74e4709e3
Lee Saville <l.w.saville@gmail.com>
2016-11-04 11:39:10 -0600

    Add custom colors depending on flash message type

---
+++ spec/features/users/user_can_login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'user can login' do
+  context 'user enters valid credentials' do
+    scenario 'they go to login_path and enter their credentails' do
+      user = Fabricate(:user)
+
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Login'
+
+      expect(page).to have_content('Successfully logged in!')
+      expect(page).to have_content("Welcome #{user.name}")
+      expect(page).to have_content('Logout')
+      expect(page).to_not have_content('Login')
+    end
+  end
+
+  context 'user enters invalid credentials' do
+  end
+end
