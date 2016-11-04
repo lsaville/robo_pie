e594be1df5beb67c1e37fa295d765d4936d6db4f
Lee Saville <l.w.saville@gmail.com>
2016-11-03 19:25:18 -0600

    Create orders_item table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104011232) do
+ActiveRecord::Schema.define(version: 20161104012142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20161104011232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
+
+  create_table "orders_items", force: :cascade do |t|
+    t.integer  "order_id"
+    t.integer  "item_id"
+    t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_items_on_item_id", using: :btree
+    t.index ["order_id"], name: "index_orders_items_on_order_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
