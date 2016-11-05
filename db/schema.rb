56e24318a93ed21f2dede1cc4fd59aeb1c13d990
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-05 15:59:15 -0600

    Add column for status to order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104012142) do
+ActiveRecord::Schema.define(version: 20161105213002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20161104012142) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "status",     default: "ordered"
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
