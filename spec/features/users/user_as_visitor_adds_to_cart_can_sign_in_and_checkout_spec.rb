b3618501e0e0482fe97881cfd576c89d113986fd
Lee Saville <l.w.saville@gmail.com>
2016-11-05 17:35:39 -0600

    Create test for story-12

---
+++ spec/features/users/user_as_visitor_adds_to_cart_can_sign_in_and_checkout_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'user as visitor adds to cart, signs in, checks out' do
+  context 'existing user' do
+    scenario 'they can place an order' do
+      user = Fabricate(:user)
+      category = Fabricate(:category)
+      items = Fabricate.times(3, :item, category: category)
+
+      visit items_path
+
+      within(".#{items[0].title}") do
+        click_button 'Add To Cart'
+      end
+      within(".#{items[1].title}") do
+        click_button 'Add To Cart'
+      end
+      within(".#{items[2].title}") do
+        click_button 'Add To Cart'
+      end
+
+      click_link 'Cart'
+      click_button 'Login or Create Account to Checkout'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Login'
+      click_link 'Cart'
+      click_button 'Checkout'
+
+      expect(current_path).to eq(orders_path)
+      expect(page).to have_content('Order was successsfully placed!')
+      expect(page).to have_content(items[0].title)
+      expect(page).to have_content(items[1].title)
+      expect(page).to have_content(items[2].title)
+    end
+  end
+end
