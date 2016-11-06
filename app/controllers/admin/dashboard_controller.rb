678a90ab8f82f4f815efb13f85fbfdbceb28bf01
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-06 16:38:29 -0700

    Find user to use in view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class Admin::DashboardController < Admin::BaseController
+  before_action :require_admin
 
   def index
-
   end
 
+  def show
+    @admin = User.find(params[:id])
+  end
+
+  def edit
+    @admin = User.find(params[:id])
+  end
 end
