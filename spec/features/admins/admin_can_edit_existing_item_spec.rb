b3c6b495bc88a739c04e91bee2fdcd781edea656
Lee Saville <l.w.saville@gmail.com>
2016-11-08 15:16:54 -0700

    Add sad path test form admin editing an item

--- spec/features/admins/admin_can_edit_existing_item_spec.rb
+++ spec/features/admins/admin_can_edit_existing_item_spec.rb
@@ -6,17 +6,12 @@ describe 'Admin can edit exiting item' do
     @item = Fabricate(:item, category: @category)
     @admin = Fabricate(:user, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-    # visit login_path
-    # fill_in 'Email', with: @admin.email
-    # fill_in 'Password', with: @admin.password
-    # click_button 'Login'
   end
 
   context 'valid params' do
     scenario 'they visit the admin/items show page and click edit' do
       visit admin_items_path
       click_button 'Edit'
-
       expect(current_path).to eq(edit_admin_item_path(@item))
 
       fill_in 'Title', with: 'new title'
@@ -27,6 +22,17 @@ describe 'Admin can edit exiting item' do
 
       expect(current_path).to eq(admin_items_path)
       expect(page).to have_content("Successfully updated item!")
+    end
+  end
+
+  context 'invalid params' do
+    scenario 'admin visits item/edit but fails to fill form' do
+      visit edit_admin_item_path(@item)
+
+      fill_in 'Title', with: ''
+      click_button 'Update Item'
+
+      expect(page).to have_content('prohibited this record from being saved')
     end
   end
 end
