983df12fb729db22f2f411e4b54148f9d84e1fac
Lee Saville <l.w.saville@gmail.com>
2016-11-08 15:09:33 -0700

    Add admin items index, edit, and update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :orders, only: [:update]
     resources :users, only: [:update, :edit]
+    resources :items, only: [:index, :edit, :update]
     get '/dashboard', to: 'dashboard#index'
     get '/:id', to: 'dashboard#show'
   end
