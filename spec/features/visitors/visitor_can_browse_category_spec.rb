01994eb957606889105c740731fac8be613ad048
Jon Kimble <jonathanbkimble@gmail.com>
2016-11-09 12:03:11 -0700

    Change test to fix capitalization error

--- spec/features/visitors/visitor_can_browse_category_spec.rb
+++ spec/features/visitors/visitor_can_browse_category_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe "Visitor can browse by category" do
   context "Categories exist" do
     scenario "they visit /:category_name" do
-      category1 = Category.create(title: "gasket lovers")
-      category2 = Category.create(title: "plastitarian")
+      category1 = Category.create(title: "Gasket lovers")
+      category2 = Category.create(title: "Plastitarian")
 
       item1 = Item.create(title: "gaskets", description: "lots of gaskets", price: 15, image: "some image", category: category1)
       item2 = Item.create(title: "flummox", description: "great stuff", price: 15, image: "image", category: category1)
